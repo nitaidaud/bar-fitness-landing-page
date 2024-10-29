c5c5a1f605685b15a45448314dc21312e0d3677c
Nitai <nitaidaud@gmail.com>
2024-10-29 11:04:28 +0200

    "Refactored About and Hero components: updated styles, typography, and layout"

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,17 +6,17 @@ export default function About() {
                     <div className="flex justify-center w-fit">
                         <img src="Images/barImage.jpeg" alt="מאמן כושר" className="rounded-full w-52" />
                     </div>
-                    <div className="text-balance flex flex-col items-start justify-center w-fit gap-4">
-                        <div className="bg-gray-700 rounded-2xl p-6 shadow-lg max-w-2xl transform hover:scale-105 transition-transform duration-300">
-                            <p className="text-3xl text-center text-white">
+                    <div className="text-balance flex flex-col items-start justify-center w-fit gap-4 mx-2 md:mx-0">
+                        <div className="bg-gray-700 rounded-2xl py-6 sm:px-6 shadow-lg max-w-2xl transform hover:scale-105 transition-transform duration-300 sm:w-11/12 mx-auto">
+                            <p className="text-2xl sm:text-3xl text-center text-white">
                                 מאות גברים ונשים ששינו את החיים שלהם בעזרת הורדת שומן
-                                <br /> 
+                                <br />
                                 והוספת מסת שריר במקומות הנכונים.
                             </p>
                         </div>
-                        
+
                         <div className="bg-primary bg-opacity-70 rounded-2xl p-6 shadow-lg max-w-2xl transform hover:scale-105 transition-transform duration-300 center mx-auto">
-                            <p className="text-3xl text-center text-white">
+                            <p className="text-2xl sm:text-3xl text-center text-white">
                                 <span className="underline font-semibold">אצלי</span>
                                 {" "}
                                 מתעסקים רק בתוצאות !
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,19 +2,15 @@
 export default function Hero() {
     return (
         <section
-            className="hero-image h-screen flex-col flex items-center justify-center text-white bg-cover bg-center  gap-10 snap-start"
+            className="hero-image h-screen flex-col flex items-center justify-center text-white bg-cover bg-center gap-10 snap-start"
         >
-            <div className="text-center bg-black/60 p-8 rounded animate__animated animate__fadeIn animate__delay-1s backdrop-blur">
-                <h2 className="text-4xl font-bold mb-4">יש לרדת במשקל ויש להתחטב !</h2>
-                <p className="mb-6 text-xl">פה אתם תראו איך אפשר לחתוך בשומן אבל גם למלא את הגוף במקומות הנכונים!</p>
+            <div className="text-center bg-black/60 py-8 px-4 rounded animate__animated animate__fadeIn animate__delay-1s backdrop-blur mx-8 md:mx-0">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4">יש לרדת במשקל ויש להתחטב !</h2>
+                <p className="mb-6 text-base text-balance md:text-xl">פה אתם תראו איך אפשר לחתוך בשומן אבל גם למלא את הגוף במקומות הנכונים!</p>
                 <a href="#contact" className="custom-btn animate__animated animate__pulse animate__infinite">
-                    התחל עכשיו
+                    להתחיל עכשיו
                 </a>
             </div>
-            {/* <div>
-                <p className="text-3xl text-center">מאות גברים ונשים ששינו את החיים שלהם בעזרת הורדת שומן <br /> והוספת מסת שריר במקומות הנכונים.</p>
-                <p className="text-3xl text-center"> <span className="underline font-semibold">אצלי</span>  מתעסקים רק בתוצאות !</p>
-            </div> */}
         </section>
     )
 }
